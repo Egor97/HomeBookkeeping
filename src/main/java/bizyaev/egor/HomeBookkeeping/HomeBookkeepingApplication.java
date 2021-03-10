a64c4ac3eebf85016ecb93ad5bbc5db06943e554
Egor Bizyaev <egorbizyaev123@gmail.com>
2021-03-10 23:58:21 +0400

    init project HomeBookkeeping

---
+++ src/main/java/bizyaev/egor/HomeBookkeeping/HomeBookkeepingApplication.java
@@ -0,0 +1,13 @@
+package bizyaev.egor.HomeBookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBookkeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBookkeepingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bizyaev/egor/HomeBookkeeping/HomeBookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package bizyaev.egor.HomeBookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
